c28111bb5eec76ee16b795bba52262774dfceae0
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-02-02 18:54:24 +0530

    day 4 of 100

---
+++ day4.cpp
@@ -0,0 +1,19 @@
+
+//day 4 of 100
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char firstname[89];
+    char lastname[89];
+    cout<<"enter your first name: ";
+    cin>>firstname;
+    cout<<"enter your last name: ";
+    cin>>lastname;
+    cout<<"your full name is : "<<firstname<<" "<<lastname;
+    
+
+    return 0;
+}
