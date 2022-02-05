338581cfacd80db6b4b8199db31f3ded3832598c
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-02-06 01:01:27 +0530

    Add files via upload
    day 8 out of 100;

---
+++ day8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class square{
+    public:
+    int length;
+    int breadth;
+    int width;
+    
+    void area(int length,int breadth){
+        cout<<"the area of the square is : "<<length*breadth<<endl;
+    }
+    void volume(int length,int breadth, int width){
+        cout<<"the volume of the square is : "<<length*breadth*width;
+    }
+};
+
+int main(){
+    
+    square s1;
+    s1.area(10,20);
+    s1.volume(30,70,50);
+    
+    return 0;
+    
+}
