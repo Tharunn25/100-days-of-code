aa84a284202c745a6aa8f146760c91badd8dfe5e
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-02-08 00:45:02 +0530

    Add files via upload
    day 10 out of 100

---
+++ day10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class value{
+   private:
+   int num;
+   public:
+   value(int number){
+       num=number;
+   }
+   void operator ++(){
+       ++num;
+   }
+   void display(){
+       cout<<num<<endl;
+   }
+    
+};
+int main(){
+    value v1(10);
+    ++v1;
+    v1.display();
+    
+    return 0;
+}
