a2e157708104bdbe64f0562e7d50365502d7e567
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-02-10 13:23:50 +0530

    Add files via upload
    day 12 out of 100

---
+++ day12.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Time{
+    private:
+    int hours;
+    int minutes;
+    
+    public:
+    Time(int h,int m){
+        hours=h;
+        minutes=m;
+    }
+    void operator ++(){
+        hours ++;
+    }
+    void operator++(int){
+        hours ++;
+    }
+    void displaytime(){
+        cout<<hours<<":"<<minutes;
+    }
+};
+
+int main()
+{
+    Time t1(6,30);
+    ++t1;
+    t1++;
+    ++t1;
+    t1.displaytime();
+
+    return 0;
+}
