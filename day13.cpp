8ee2a7ca896cc7cae069061705f06c3f3a01fcf9
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-02-11 23:12:23 +0530

    Add files via upload
    day 13 out of 100

---
+++ day13.cpp
@@ -0,0 +1,22 @@
+//day 13 out of 100#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float arr[5];
+    float*ptr;
+    
+
+    
+    for(int i=0;i<5;i++){
+        cout<<&arr[i]<<endl;
+    }
+    ptr=arr;
+    
+    for(int j=0;j<5;j++){
+        cout<<ptr+j<<endl;
+    }
+    return 0;
+}
