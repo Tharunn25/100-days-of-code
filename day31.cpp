f96dcde5ce244341952e036fd9b2f7ffb19cdc51
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-03-02 23:54:15 +0530

    Add files via upload
    day 31 out of 100

---
+++ day31.cpp
@@ -0,0 +1,17 @@
+//day 31 out of 100
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int nums[5] = {1,2,42,52,53};
+    int max = nums[0];
+    for(int i=1;i<5;++i){
+        if(nums[i]<max){
+            max = nums[i];
+        }
+        cout<<"The smallest element among the following is : "<<max;
+    }
+    return 0;
+}
