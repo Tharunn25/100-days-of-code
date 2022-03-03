35d976e26fc52059fa9ef288cac7a63e910d711e
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-03-03 18:12:53 +0530

    Add files via upload
    day 32 out of 100

---
+++ day32.cpp
@@ -0,0 +1,22 @@
+//day 32 out of 100
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int arr[6]={11,12,13,14,15,16};
+	
+	for(int i=0;i<6;i++)
+		cout<<arr[i]<<" ";
+	
+cout<<endl;
+	
+	cout<<"By Other Method:"<<endl;
+	for(int i=0;i<6;i++)
+		cout<<i[arr]<<" ";
+	
+	cout<<endl;
+
+	return 0;
+}
+
