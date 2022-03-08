711144945007fd734d175e804c120e91e1188cd2
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-03-08 23:44:58 +0530

    Add files via upload
    day 37 out of 100

---
+++ day37.cpp
@@ -0,0 +1,43 @@
+//day 37 out of 100
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n-1; i++)	
+	
+
+	for (j = 0; j < n-i-1; j++)
+		if (arr[j] > arr[j+1])
+			swap(&arr[j], &arr[j+1]);
+}
+
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	int arr[] = {74, 14, 25, 12, 21, 11, 92};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	bubbleSort(arr, n);
+	cout<<"Sorted array: \n";
+	printArray(arr, n);
+	return 0;
+}
+
