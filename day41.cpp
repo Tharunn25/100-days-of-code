7c521a281cf3e56caaa564c7539b508f8cb81e4f
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-03-12 14:55:42 +0530

    Add files via upload
    day 41 out of 100

---
+++ day41.cpp
@@ -0,0 +1,55 @@
+//day 41 out of 100
+#include <iostream>
+
+using namespace std;
+
+void selectionsort(int arr[])
+{
+    for(int i=0;i<4;i++)
+    {
+        int min = i;
+        
+        for(int j=i+1;j<5;j++){
+            if(arr[j]<arr[min])
+            {
+                min=j;
+            }
+        }
+        if(min != i){
+            int temp = arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
+
+int main()
+{
+    int myarr[5];
+    
+    cout<<"Enter any five numbers : "<<" "<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cin>>myarr[i];
+    }
+    
+    cout<<"unsorted array : "<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cout<<myarr[i]<<" ";
+    }
+    
+    cout<<endl;
+    
+    
+    selectionsort(myarr);
+    
+    cout<<"The sorted array is : "<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cout<<myarr[i]<<" ";
+    }
+    
+
+    return 0;
+}
