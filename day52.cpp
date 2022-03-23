109fecfa036089e2c1e9ff1eec36c39c85448686
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-03-23 18:23:56 +0530

    Add files via upload
    day 52 out of 100

---
+++ day52.cpp
@@ -0,0 +1,44 @@
+// day 52 out of 100
+
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+void insert(int new_data)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node *));
+    new_node->data = new_data;
+    new_node->next = head;
+    head = new_node;
+}
+
+void display()
+{
+    struct Node *ptr;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        cout << ptr->data << "->";
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    insert(4);
+    insert(9);
+    insert(2);
+    insert(7);
+    cout << "The linked list is : ";
+    display();
+
+    return 0;
+}
