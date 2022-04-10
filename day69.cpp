182c7fb13deebdb27318b2e15d729d1934db45f3
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-04-10 23:12:40 +0530

    Add files via upload
    day 69 out of 100

---
+++ day69.cpp
@@ -0,0 +1,25 @@
+// day 69 out of 100
+
+// C++ program to illustrate Array Name as Pointers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void geeks()
+{
+    int val[3] = {5, 10, 15};
+
+    int *ptr;
+
+    ptr = val;
+    cout << "Elements of the array are: ";
+    cout << ptr[0] << " " << ptr[1] << " " << ptr[2];
+
+    return;
+}
+
+int main()
+{
+    geeks();
+    return 0;
+}
