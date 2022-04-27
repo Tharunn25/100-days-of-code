4b4b31dbacc3b54648cd5ff7428f2b911db41998
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-04-27 22:41:26 +0530

    Add files via upload
    day 85 out of 100

---
+++ day85.cpp
@@ -0,0 +1,13 @@
+// day 84 out of 100
+
+// cpp program to print the values at index position
+
+void print(int ar[], int n)
+{
+
+    // code here
+    for (int i = 0; i < n; i = i + 2)
+    {
+        cout << ar[i] << " ";
+    }
+}
