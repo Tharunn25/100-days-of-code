741659140e119c525ae08773eceef2e12cfb77f8
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-05-02 21:29:56 +0530

    Add files via upload
    day 89 out of 100

---
+++ day89.cpp
@@ -0,0 +1,17 @@
+// day 89 out of 100
+
+// gfg array problem
+
+class Solution
+{
+public:
+    int findElementAtIndex(int a[], int n, int key)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] = a[key])
+                ;
+            return a[key];
+        }
+    }
+};
