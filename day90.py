1d6ae6ac3d0dc6264d9102ab1247bbecce8368d1
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-05-03 22:46:21 +0530

    Add files via upload
    day 90 out of 100

---
+++ day90.py
@@ -0,0 +1,11 @@
+# day 90 out of 100
+# learning python
+
+# python program to print sum of n numbers
+
+numbers = [1, 2, 4, 5, 5, 1, 25, 52]
+
+sum = 0
+for val in numbers:
+    sum += val
+print("The sum of the given numbers is : ", sum)
