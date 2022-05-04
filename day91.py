f7411a7618bd647fad774763ea05dd87b53dffb9
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-05-04 19:57:33 +0530

    Add files via upload
    day 91 out of 100

---
+++ day91.py
@@ -0,0 +1,9 @@
+# day 91 out of 100
+# learing pyton tkinter gui
+
+import tkinter
+m = tkinter.Tk()
+m.title('what do you mean')
+button = tkinter.Button(m, text='stop', width=25, command=m.destroy)
+button.pack()
+m.mainloop()
