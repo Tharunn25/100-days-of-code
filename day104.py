fc390b06069d6d37cb00a09803d9db29dac6b013
Tharunn25 <98308855+Tharunn25@users.noreply.github.com>
2022-05-18 16:05:36 +0530

    Add files via upload
    day 104 out of 100

---
+++ day104.py
@@ -0,0 +1,27 @@
+# day 104 out of 100
+# tkinter
+import tkinter as tk
+from tkinter import ttk
+
+
+def greet():
+    print(f"hello, {user_name.get() or 'world'}")
+
+
+root = tk.Tk()
+root.title("hello world")
+
+user_name = tk.StringVar()
+
+
+name_label = ttk.Label(root, text="name: ")
+name_label.pack(side="left", padx=(0, 10))
+name_entry = ttk.Entry(root, width=15, textvariable=user_name)
+name_entry.pack(side="left")
+name_entry.focus()
+
+greet_button = ttk.Button(root, text="greet", command=greet)
+greet_button.pack(side="left", fill="x", expand=True)
+
+
+root.mainloop()
